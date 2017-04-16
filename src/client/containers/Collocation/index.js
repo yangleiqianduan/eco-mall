8f5c7eb33f04e0cc6c16a8421759b37927c39576
zhumin <zhumin@lianjia.com>
2017-04-17 07:55:10 +0800

    modify 新增collocation页面

--- src/client/containers/Collocation/index.js
+++ src/client/containers/Collocation/index.js
@@ -1,8 +1,12 @@
 import React, { PureComponent } from 'react'
 
+import CSSModules from 'react-css-modules'
+import styles from './index.styl'
+
+@CSSModules(styles, {allowMultiple: true})
 export class Collocation extends PureComponent {
   render () {
-    return <div>Collocation</div>
+    return <div styleName='wrap'>Collocation</div>
   }
 }
 
