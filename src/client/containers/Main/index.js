dd533e9507e396d1ffe3872bb6fc38a9cb4f3676
fe_wangcong <wangcong@lianjia.com>
2017-04-20 14:07:19 +0800

    update webpack build config

--- src/client/containers/Main/index.js
+++ src/client/containers/Main/index.js
@@ -18,6 +18,7 @@ import { getCateoryList } from 'actions/'
 export class Main extends PureComponent {
   componentDidMount () {
     this.props.dispatch(getCateoryList())
+    window.$ULOG.send('10000', {type: 'pv', page: 'home'})
   }
   componentWillMount () {
     const { location, routes } = this.props
