9e4b78466c31e54b301065703e5ae5173dfa67f0
zhumin <zhumin@lianjia.com>
2017-04-21 18:24:49 +0800

    modify 调整底部按钮icon

--- src/client/components/FootBar/index.js
+++ src/client/components/FootBar/index.js
@@ -3,6 +3,7 @@ import CSSModules from 'react-css-modules'
 import styles from './index.styl'
 
 import Button from 'components/Button/'
+import Icon from 'components/Icons'
 
 @CSSModules(styles, {allowMultiple: true})
 export default class extends PureComponent {
@@ -10,7 +11,18 @@ export default class extends PureComponent {
     const { onBuy } = this.props
     return <div styleName='wrap'>
       <div styleName='content'>
-        <div styleName='callUs'>icon</div>
+        <div styleName='icons'>
+          <div styleName='item'>
+            <Icon icon = {'listener'} width={18} stroke = {'#FFF'}/>
+          </div>
+          <div styleName='item cart'>
+            <Icon icon = {'cart'} width={18} stroke = {'#FFF'}/>
+            <em>2</em>
+          </div>
+          <div styleName="item addCart">
+            加入购物车
+          </div>
+        </div>
         <div styleName='buyArea'><Button bright onClick={onBuy}>立即预约</Button></div>
       </div>
     </div>
