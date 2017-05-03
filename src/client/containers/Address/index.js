39e05bc8611fbf233b446128b3193ce945169af7
fe_wangcong <wangcong@lianjia.com>
2017-05-03 16:07:04 +0800

    add switch component

--- src/client/containers/Address/index.js
+++ src/client/containers/Address/index.js
@@ -4,11 +4,22 @@ import { connect } from 'react-redux'
 import CSSModules from 'react-css-modules'
 import styles from './index.styl'
 
+import Switch from 'components/Switch/'
+
 @CSSModules(styles, { allowMultiple: true })
 export class Address extends PureComponent {
+  state = {
+    isDefault: false
+  }
   render () {
     return <div styleName='wrap'>
-      收货地址详情
+      <section styleName='panel'>
+        收货地址详情
+      </section>
+      <section styleName='panel'>
+        <div styleName='long'>设置为默认地址</div>
+        <Switch value={this.state.isDefault} onChange={(v) => this.setState({isDefault: v})} />
+      </section>
     </div>
   }
 }
