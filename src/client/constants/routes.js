2ebbbf9ce6af52aa92c7b0af02cf7d289126c6c0
fe_wangcong <wangcong@lianjia.com>
2017-05-15 12:08:21 +0800

    use chunk to split route component

--- src/client/constants/routes.js
+++ src/client/constants/routes.js
@@ -1,135 +1,147 @@
-import Home from 'containers/Home/'
-import Detail from 'containers/Detail/'
-import Subject from 'containers/Subject/'
-import Result from 'containers/Result/'
-import Collocation from 'containers/Collocation/'
-import Success from 'containers/Success/'
-import Shoppingcart from 'containers/Shoppingcart/'
-import AddressList from 'containers/AddressList/'
-import Address from 'containers/Address/'
-import OrderConfirm from 'containers/OrderConfirm/'
-import OrderList from 'containers/OrderList/'
-import Order from 'containers/Order/'
-import Logistics from 'containers/Logistics/'
-import Want from 'containers/Want/'
-import VoteList from 'containers/VoteList/'
-import Vote from 'containers/Vote/'
-import VoteResult from 'containers/VoteResult/'
-import ErrorPage from 'containers/ErrorPage'
-import Icons from 'components/Icons/demo/'
+// import Home from 'containers/Home/'
+// import Detail from 'containers/Detail/'
+// import Subject from 'containers/Subject/'
+// import Result from 'containers/Result/'
+// import Collocation from 'containers/Collocation/'
+// import Success from 'containers/Success/'
+// import Shoppingcart from 'containers/Shoppingcart/'
+// import AddressList from 'containers/AddressList/'
+// import Address from 'containers/Address/'
+// import OrderConfirm from 'containers/OrderConfirm/'
+// import OrderList from 'containers/OrderList/'
+// import Order from 'containers/Order/'
+// import Logistics from 'containers/Logistics/'
+// import Want from 'containers/Want/'
+// import VoteList from 'containers/VoteList/'
+// import Vote from 'containers/Vote/'
+// import VoteResult from 'containers/VoteResult/'
+// import ErrorPage from 'containers/ErrorPage'
 
 export default [
   {
     path: '/',
     exact: true,
-    component: Home,
+    // component: Home,
+    chunk: () => System.import('containers/Home/'),
     title: '链家·优品'
   },
   {
     path: '/search',
     exact: true,
-    component: Result,
+    // component: Result,
+    chunk: () => System.import('containers/Result/'),
     title: '优品目录'
   },
   {
     path: '/item',
     exact: true,
-    component: Detail,
+    // component: Detail,
+    chunk: () => System.import('containers/Detail/'),
     title: '商品详情'
   },
   {
     path: '/collocation',
     exact: true,
-    component: Collocation,
+    // component: Collocation,
+    chunk: () => System.import('containers/Collocation/'),
     title: '搭配详情'
-  }, {
-    path: '/subject',
-    exact: true,
-    component: Subject,
-    title: '专题'
   },
+  // {
+  //   path: '/subject',
+  //   exact: true,
+  //   // component: Subject,
+  //   chunk: () => System.import('containers/Subject/'),
+  //   title: '专题'
+  // },
   {
     path: '/shoppingcart',
     exact: true,
-    component: Shoppingcart,
+    // component: Shoppingcart,
+    chunk: () => System.import('containers/Shoppingcart/'),
     title: '购物车'
   },
   {
     path: '/addressList',
     exact: true,
-    component: AddressList,
+    // component: AddressList,
+    chunk: () => System.import('containers/AddressList/'),
     title: '地址管理'
   },
   {
     path: '/address',
     exact: true,
-    component: Address,
+    // component: Address,
+    chunk: () => System.import('containers/Address/'),
     title: '新建地址'
   },
   {
     path: '/orderConfirm',
     exact: true,
-    component: OrderConfirm,
+    // component: OrderConfirm,
+    chunk: () => System.import('containers/OrderConfirm/'),
     title: '确认订单'
   },
   {
     path: '/orderList',
     exact: true,
-    component: OrderList,
+    // component: OrderList,
+    chunk: () => System.import('containers/OrderList/'),
     title: '订单列表'
   },
   {
     path: '/order',
     exact: true,
-    component: Order,
+    // component: Order,
+    chunk: () => System.import('containers/Order/'),
     title: '订单详情'
   },
   {
     path: '/logistics',
     exact: true,
-    component: Logistics,
+    // component: Logistics,
+    chunk: () => System.import('containers/Logistics/'),
     title: '追踪物流'
   },
   {
     path: '/want',
     exact: true,
-    component: Want,
+    // component: Want,
+    chunk: () => System.import('containers/Want/'),
     title: '需求登记'
   },
-  {
-    path: '/success',
-    exact: true,
-    component: Success,
-    title: '预约成功'
-  },
+  // {
+  //   path: '/success',
+  //   exact: true,
+  //   // component: Success,
+  //   chunk: () => System.import('containers/Success/'),
+  //   title: '预约成功'
+  // },
   {
     path: '/voteList',
     exact: true,
-    component: VoteList,
+    // component: VoteList,
+    chunk: () => System.import('containers/VoteList/'),
     title: '投票专栏'
   },
   {
     path: '/vote',
     exact: true,
-    component: Vote,
+    // component: Vote,
+    chunk: () => System.import('containers/Vote/'),
     title: '投票'
   },
   {
     path: '/voteResult',
     exact: true,
-    component: VoteResult,
+    // component: VoteResult,
+    chunk: () => System.import('containers/VoteResult/'),
     title: '投票结果'
   },
   {
     path: '/errorPage',
     exact: true,
-    component: ErrorPage,
+    // component: ErrorPage,
+    chunk: () => System.import('containers/ErrorPage/'),
     title: '错误'
-  },
-  {
-    path: '/icon',
-    exact: true,
-    component: Icons,
-    title: 'icon示例'
   }
 ]
