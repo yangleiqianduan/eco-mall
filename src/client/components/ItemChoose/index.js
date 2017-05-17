dba91f8c835942d1c3ea28f4baf672cd888f830c
由志佳 <yushikei@youzhijiadeMacBook-Pro.local>
2017-05-17 17:18:36 +0800

    订单详情页样式

--- src/client/components/ItemChoose/index.js
+++ src/client/components/ItemChoose/index.js
@@ -9,11 +9,11 @@ export default class extends PureComponent {
 
     return <section styleName='wrap'>
       <div styleName='label first' onClick={onShowChoose}>
-        <div styleName='long'>已选：<span styleName='value'>{skuChoose || '请选择规格参数'}</span></div>
+        <div styleName='long'><span styleName='value'>已选：</span><span styleName='value'>{skuChoose || '请选择规格参数'}</span></div>
         <div><span styleName='dot' /><span styleName='dot' /><span styleName='dot' /></div>
       </div>
       <div styleName='label second' onClick={onShowParam}>
-        <div styleName='long'>产品参数：<span styleName='value'>{params.map(item => item.spu_attribute_key_name + ' ' + item.spu_attribute_value_info.map(item => item.value).join(','))[0]}...</span></div>
+        <div styleName='long'><span styleName='value'>产品参数：</span><span styleName='value'>{params.map(item => item.spu_attribute_key_name + ' ' + item.spu_attribute_value_info.map(item => item.value).join(','))[0]}...</span></div>
         <div styleName='dotWrap'><span styleName='dot' /><span styleName='dot' /><span styleName='dot' /></div>
       </div>
     </section>
