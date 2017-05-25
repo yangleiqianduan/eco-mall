ee3f4604ef7d1a1262ffb59f1c17d0dd2ea54201
由志佳 <yushikei@youzhijiadeMacBook-Pro.local>
2017-05-25 14:44:05 +0800

    商品详情图片通栏

--- src/client/containers/Order/index.js
+++ src/client/containers/Order/index.js
@@ -78,7 +78,7 @@ export class OrderList extends PureComponent {
           }       
         </div>
         {
-          isNeedPay
+          !isNeedPay
           ? <div>
               <div styleName="contect bgWhite" onClick={(e) => this.confirmTel(e)}>
                 <Icon icon='listener' width="16"/><span styleName='text'>联系客服</span>
