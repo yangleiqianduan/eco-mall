31d59b727543f433cc82ee843507620642aad3d1
fe_wangcong <wangcong@lianjia.com>
2017-06-13 15:59:33 +0800

    make broswer history fix to wx config

--- src/client/common/bridge.js
+++ src/client/common/bridge.js
@@ -1,4 +1,4 @@
-import { fetch } from './fetch'
+import fetch from './fetch'
 import * as api from 'constants/api'
 import { defaultSharedImg } from 'constants/img'
 
@@ -22,6 +22,7 @@ export const setShare = ({title, description, img, url}, isShareImg) => {
       requestUrl: url || window.location.origin
     }))
   } else {
+    // 微信下分享
     const shareArgument = {
       title: title || '一站式家居平台-链家家居', // 分享标题
       desc: description || '贵一点，好很多的，链家家居为你工厂直采高质低价家居商品',
@@ -30,10 +31,21 @@ export const setShare = ({title, description, img, url}, isShareImg) => {
       success: () => {},
       cancel: () => {}
     }
-    // 微信下分享
-    window.wx.onMenuShareTimeline(shareArgument)
-    window.wx.onMenuShareAppMessage(shareArgument)
-    window.wx.onMenuShareQQ(shareArgument)
+    fetch(api.getWxToken, {param: {url: window.location.href.split('#')[0]}})
+    .then(res => {
+      const result = res.data
+      window.wx.config({
+        debug: window.env === 'dev', // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: result.appId, // 必填，公众号的唯一标识
+        timestamp: result.timestamp, // 必填，生成签名的时间戳
+        nonceStr: result.nonceStr, // 必填，生成签名的随机串
+        signature: result.signature, // 必填，签名，见附录1
+        jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+      })
+      window.wx.onMenuShareTimeline(shareArgument)
+      window.wx.onMenuShareAppMessage(shareArgument)
+      window.wx.onMenuShareQQ(shareArgument)
+    })
   }
 }
 
