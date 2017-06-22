52268d8d929cfc7c9f2021ce9ad90d49a40ae9e4
由志佳 <yushikei@youzhijiadeMacBook-Pro.local>
2017-06-22 11:41:48 +0800

    代付款取消订单接口联调

--- src/client/actions/order.js
+++ src/client/actions/order.js
@@ -25,10 +25,10 @@ export const getOrderDetail = param => async dispatch => {
 
 export const cancelOrder = (id) => async dispatch => {
   dispatch(UPDATE_LOADING_ACTION(true))
-  const result = await fetch(api.cancelOrder, {param: {order_id: id}})
+  const result = await fetch(api.cancelOrder, {param: {order_id: id}, method: 'post', formData: true})
   if (result.code === '1') {
     dispatch(UPDATE_LOADING_ACTION(false))
-    dispatch(getOrderDetail({param: {order_id: id}, method: 'post', formData: true}))
+    dispatch(getOrderDetail({param: {order_id: id}}))
   } else {
     dispatch(UPDATE_LOADING_ACTION(false))
     dispatch(showToast(result.msg || '取消失败'))
